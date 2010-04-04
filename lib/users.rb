853fadb245ea94d0ccc6f0ebbadc0bd47475e37a
Bruno Michel <bmichel@menfin.info>
2010-04-04 15:24:47 +0200

    Anonymous user is created by db:seed, so don't import it

--- lib/users.rb
+++ lib/users.rb
@@ -9,26 +9,6 @@
 puts "\n____( Users )___________________________________________________________________\n"
 
 ROR.transaction do
-  ROR[:users].insert(
-    :id         => 1,
-    :name       => "Anonyme",
-    :homesite   => nil,
-    :jabber_id  => nil,
-    :role       => "inactive",
-    :created_at => "NOW()".lit,
-    :updated_at => "NOW()".lit
-  )
-  ROR[:accounts].insert(
-    :id           => 1,
-    :user_id      => 1,
-    :login        => 'Anonyme',
-    :email        => 'anonyme@linuxfr.org',
-    :state        => 'deleted',
-    :old_password => nil,
-    :created_at   => 'NOW()'.lit,
-    :updated_at   => 'NOW()'.lit
-  )
-
   users = TPL[:users].select(:id, :lname, :fname, :login, :email, :homesite, :jabber_id, :status, :created, :level, :passwd)
   users.where('id != 1 AND status != 0').each do |user|
     id    = user[:id]
